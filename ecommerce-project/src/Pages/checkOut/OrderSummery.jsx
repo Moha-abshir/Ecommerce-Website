62d75789201181fb3cb138f2023a983f46f394b2
Mohammed Abshir <moabshir06@gmail.com>
2025-10-25 17:11:39 +0300

    Create delete cart item feature

--- ecommerce-project/src/Pages/checkOut/OrderSummery.jsx
+++ ecommerce-project/src/Pages/checkOut/OrderSummery.jsx
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import axios from 'axios'
 import { formatMoney } from '../../utils/money';
 import { DeliveryOptions } from './DeliveryOptions';
 
@@ -7,8 +8,12 @@ export function OrderSummery({deliveryOptions, cart, loadCart}){
       <div className="order-summary">
         {deliveryOptions.length> 0 && cart.map((cartItem)=>{
           const selectedDeliveryOption = deliveryOptions.find((deliveryOption)=>{
-            return deliveryOption.id === cartItem.deliveryOptionId
-          });
+            return deliveryOption.id === cartItem.deliveryOptionId});
+
+            const deleteCartItem = async()=>{
+              await axios.delete(`/api/cart-items/${cartItem.productId}`)
+              await loadCart();
+            }
           return(
             
             <div className="cart-item-container" key={cartItem.productId}>
@@ -34,7 +39,9 @@ export function OrderSummery({deliveryOptions, cart, loadCart}){
                     <span className="update-quantity-link link-primary">
                       Update
                     </span>
-                    <span className="delete-quantity-link link-primary">
+                    <span className="delete-quantity-link link-primary"
+                      onClick={deleteCartItem}
+                    >
                       Delete
                     </span>
                   </div>
