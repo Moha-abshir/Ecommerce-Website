139936d73c3371777de9db57f09996304d1bbd5a
Mohammed Abshir <moabshir06@gmail.com>
2025-10-27 16:32:43 +0300

    made the "Add To Cart" button in the orders page interactive

--- ecommerce-project/src/Pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/Pages/orders/OrdersPage.jsx
@@ -1,16 +1,16 @@
-import {Fragment, useEffect, useState} from 'react'
-import { Link } from "react-router-dom";
+import { useEffect, useState} from 'react'
 import { Header } from "../../Shared-Components/header";
 import dayjs from 'dayjs';
 import { formatMoney } from '../../utils/money';
 import './OrdersPage.css'
 import axios from 'axios';
+import { OrderProducts } from './OrderProducts';
 
-export function OrdersPage({cart}) {
+export function OrdersPage({cart, loadCart}) {
   const [orders, setOrders] = useState([]);
   useEffect(()=>{
     axios.get('/api/orders?expand=products')
-    .then(res => setOrders(res.data))
+    .then(res => setOrders(res.data));
   })
 
   return (
@@ -49,34 +49,7 @@ export function OrdersPage({cart}) {
                   {order.products.map((orderProducts)=>{
 
                     return(
-                      <Fragment key={orderProducts.prodctId}>
-                        <div className="product-image-container">
-                          <img src={orderProducts.product.image} />
-                        </div>
-
-                        <div className="product-details">
-                          <div className="product-name">
-                            {orderProducts.product.name}
-                          </div>
-                          <div className="product-delivery-date">Arriving on: {dayjs(orderProducts.estimatedDeliveryTimeMs).format('MMMM D')}</div>
-                          <div className="product-quantity">Quantity: {orderProducts.quantity}</div>
-                          <button className="buy-again-button button-primary">
-                            <img
-                              className="buy-again-icon"
-                              src="images/icons/buy-again.png"
-                            />
-                            <span className="buy-again-message">Add to Cart</span>
-                          </button>
-                        </div>
-
-                        <div className="product-actions">
-                          <Link to={`/tracking/${order.id}/${orderProducts.product.id}`}>
-                            <button className="track-package-button button-secondary">
-                              Track package
-                            </button>
-                          </Link>
-                        </div>
-                      </Fragment>
+                      <OrderProducts orderProducts={orderProducts} order={order} key={orderProducts.productId} loadCart={loadCart}/>
                     )
                   })}
                 </div>
